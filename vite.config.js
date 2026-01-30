b95a329624be6494b86023b08f7f17ba3bbf2576
MehdiaHumais <sisterlittle64@gmail.com>
2026-01-30 12:52:41 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +7,5 @@ export default defineConfig({
     port: 5173, // Explicitly set the port
     strictPort: true, // Exit if port is already in use
   },
+
 })
